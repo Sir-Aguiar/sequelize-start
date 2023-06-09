af7a6b0a0e08bb1d5df836313e6840b6c146030b
Sir-Aguiar <felipeaguiarficdev@gmail.com>
2023-06-09 10:40:34 -0400

    another machine config

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,7 +6,6 @@ interface PsqlCredentials extends Options {}
 const Northwind = new Sequelize(CREDENTIALS_CONFIG as PsqlCredentials);
 
 export default Northwind;
-
 /*
 	The define section in this configuration file is just to previne some errors. Since our example database (Northwind) doesn't have createdAt or updatedAt columns, we need to say to Sequelize to don't expect it to be there.
 
--- src/database/models/territories.ts
+++ src/database/models/territories.ts
@@ -3,36 +3,35 @@ import Northwind from "../database";
 import { Region } from "./region";
 
 export const Territories = Northwind.define("territories", {
-  territory_id: {
-    allowNull: false,
-    primaryKey: true,
-    type: DataTypes.STRING(20),
-  },
-  territory_description: {
-    allowNull: false,
-    type: DataTypes.STRING(60),
-  },
-  region_id: {
-    type: DataTypes.SMALLINT,
-    allowNull: false,
-  },
+	territory_id: {
+		allowNull: false,
+		primaryKey: true,
+		type: DataTypes.STRING(20),
+	},
+	territory_description: {
+		allowNull: false,
+		type: DataTypes.STRING(60),
+	},
+	region_id: {
+		type: DataTypes.SMALLINT,
+		allowNull: false,
+	},
 });
 
 Territories.belongsTo(Region, {
-  constraints: true,
-  foreignKey: "region_id",
+	constraints: true,
+	foreignKey: "region_id",
 });
 
 Region.hasMany(Territories, {
-  foreignKey: "region_id",
+	foreignKey: "region_id",
 });
 
-
 /* (async () => {
-  const result = await Territories.findByPk("01581", { include: Region });
-  console.log(result?.toJSON());
-})(); */
-
+	const result = await Territories.findByPk("01581", { include: Region });
+	console.log(result?.toJSON());
+})();
+ */
 /*
 
   (errado)
@@ -43,3 +42,4 @@ Region.hasMany(Territories, {
   Um território, pertence apenas a uma região
   Uma região, possui vários territórios
 */
+
